3050d2e4d7b1bf1d455d586aa6d3a7e833d7d0fd
Jiakun Yan <jiakunyan1998@gmail.com>
2020-09-30 16:58:13 +0800

    add argument for push

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 # if you are using a VPN, you will need the --network host option
 # You probably also need to restart docker after using VPN by
 # sudo systemctl restart docker
 docker build --network host -t jiakuny/gasnetex:latest .
+
+if [ "$1" == "push" ]
+then
+  docker push jiakuny/gasnetex:latest
+fi
